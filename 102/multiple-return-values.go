b6a9f00646374ab4beace4ee208e73daee3e43f3
mustafa.ozcan <wustafa.ozcan@gmail.com>
2023-12-05 21:25:53 +0300

    multiple-return-values.go has been added

--- 102/multiple-return-values.go
+++ 102/multiple-return-values.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func multipleReturn() (int, int) {
+func twoReturnValues() (int, int) {
 	return 3, 7
 }
 
+func threeReturnValues() (int, string, int) {
+	return 1, "Adana", 2
+}
+
 func main() {
-	first, second := multipleReturn()
+	first, second := twoReturnValues()
 	fmt.Println("first:", first, "second:", second)
 
-	_, sec := multipleReturn()
+	_, sec := twoReturnValues()
 	fmt.Println("second:", sec)
+
+	fir, _, third := threeReturnValues()
+	fmt.Println("fir:", fir, "third:", third)
 }
