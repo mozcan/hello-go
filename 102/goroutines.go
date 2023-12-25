904d447be460b64828feff2749336008772fdb8f
mustafa.ozcan <wustafa.ozcan@gmail.com>
2023-12-26 01:00:18 +0300

    Goroutine example has been updated

--- 102/goroutines.go
+++ 102/goroutines.go
@@ -5,22 +5,16 @@ import (
 	"time"
 )
 
-func f(arg string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(arg, " i :", i)
+func count(from string) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(time.Millisecond * 500) // Yarı saniye bekleme
 	}
 }
-
 func main() {
-	f("Argument")
-
-	go f("goroutine")
-
-	go func(msg string) {
-		fmt.Println(msg)
-	}("going")
-
-	time.Sleep(time.Second)
-	fmt.Println("done")
-
+	// İki farklı fonksiyonu aynı anda çalıştırma (Goroutines)
+	go count("Gorutin 1")
+	go count("Gorutin 2")
+	// Ana fonksiyonun kapanmasını beklemek için bir bekleme süresi ekleyelim
+	time.Sleep(time.Second * 3)
 }
