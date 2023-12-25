16213a844fc04134237df05d5b7ceacd5b74b222
mustafa.ozcan <wustafa.ozcan@gmail.com>
2023-12-26 00:13:11 +0300

    Goroutine example has been added

--- 102/goroutines.go
+++ 102/goroutines.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(arg string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(arg, " i :", i)
+	}
+}
+
+func main() {
+	f("Argument")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+}
