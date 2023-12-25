3005624129ca49eae6f0fa60440f7afda0b79f69
mustafa.ozcan <wustafa.ozcan@gmail.com>
2023-12-26 00:14:13 +0300

    Generics example has been added

--- 102/generics.go
+++ 102/generics.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+	var number int = 5
+	fmt.Println(plusOne(number))
+	var fnumber float64 = 3.1
+	fmt.Println(plusOne(fnumber))
+}
+
+func plusOne[num int | float64](number num) num {
+	return number + 1
+}
